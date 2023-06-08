0721f93d78e05536fb9f1c7a6b10068ab387f025
Terariver <jared.javillo@obf.ateneo.edu>
2023-06-08 16:39:27 +0800

    last update

--- lib/employee_email/cubit/employee_email_cubit.dart
+++ lib/employee_email/cubit/employee_email_cubit.dart
@@ -5,7 +5,7 @@ import 'package:employee_email/employee_email/model/employee_model.dart';
 /// A [Cubit] which manages an [String] as its state.
 /// {@endtemplate}
 class EmployeeEmailCubit extends Cubit<String> {
-  /// {@macro counter_cubit}
+  /// {@macro employee_email_cubit}
   EmployeeEmailCubit() : super("No Filter");
 
   void changeFilter(String filter,List<EmployeeModel> employeeList, List<EmployeeModel> filteredEmployees) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:employee_email/app.dart';
 import 'package:employee_email/employee_email_observer.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 void main() async {
-  //Bloc.observer = const EmployeeEmailObserver();
+  Bloc.observer = const EmployeeEmailObserver();
   runApp(const MyApp());
 }
